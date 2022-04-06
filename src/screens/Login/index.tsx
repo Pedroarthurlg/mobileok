49b05baaef665bcdc88de44eac9faf37bb743b44
lazarodu <lazarodu@gmail.com>
2022-04-06 09:43:43 -0300

    Login Input

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { View, Text, TextInput, KeyboardAvoidingView } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default function Login() {
   return (
     <View>
       <KeyboardAvoidingView>
         <Text>Login</Text>
-        <View></View>
+        <View>
+          <MaterialIcons name="email" size={24} color="black" />
+          <TextInput
+            placeholder="E-mail"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+        </View>
       </KeyboardAvoidingView>
     </View>
   );
