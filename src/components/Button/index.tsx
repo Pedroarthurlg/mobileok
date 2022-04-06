df065cad12d2790889197b8ae55f2c7788a5da07
lazarodu <lazarodu@gmail.com>
2022-04-06 10:04:42 -0300

    Arquivo Styles do Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { TouchableOpacity, Text } from "react-native";
+import { ButtonProps } from "../../interfaces/Button.interface";
+import styles from "./styles";
 
-export default function Button() {
+export default function Button({onPress, title, type, ...rest}:ButtonProps) {
   return (
-    <TouchableOpacity>
+    <TouchableOpacity style={
+      type == "green" ? styles
+    }>
       <Text></Text>
     </TouchableOpacity>
   );
