82ee77528e3e0a422ce5fde834c4435c0a2e1b0a
lazarodu <lazarodu@gmail.com>
2022-04-13 09:58:47 -0300

    Aula do dia 13/04/2022

---
+++ src/screens/Cadastrar/index.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  KeyboardAvoidingView,
+  ImageBackground,
+} from "react-native";
+import { MaterialIcons, Entypo, Ionicons } from "@expo/vector-icons";
+import Button from "../../components/Button";
+import styles from "./styles";
+
+export default function Cadastrar() {
+  async function handleSignIn() {
+    console.log("Login");
+  }
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={require("../../assets/fundo.png")}
+        style={styles.container}
+      >
+        <KeyboardAvoidingView>
+          <Text style={styles.title}>Login</Text>
+          <View style={styles.formRow}>
+            <Ionicons name="person" style={styles.icon} />
+            <TextInput style={styles.input} placeholder="Nome" />
+          </View>
+          <View style={styles.formRow}>
+            <MaterialIcons name="email" style={styles.icon} />
+            <TextInput
+              style={styles.input}
+              placeholder="E-mail"
+              keyboardType="email-address"
+              autoCapitalize="none"
+            />
+          </View>
+          <View style={styles.formRow}>
+            <Entypo name="key" style={styles.icon} />
+            <TextInput
+              style={styles.input}
+              placeholder="Senha"
+              secureTextEntry={true}
+              autoCapitalize="none"
+            />
+          </View>
+          <Button title="Login" type="purple" onPress={handleSignIn} />
+          <Button title="Cadastre-se" type="green" onPress={handleSignIn} />
+        </KeyboardAvoidingView>
+      </ImageBackground>
+    </View>
+  );
+}
