a8a33e2768d8338ba5b1a4e0750937e12b4c5d67
lazarodu <lazarodu@gmail.com>
2022-04-13 09:38:45 -0300

    Aula do dia 13/04/2022

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text, TextInput, KeyboardAvoidingView } from "react-native";
 import { MaterialIcons, Entypo } from "@expo/vector-icons";
 import Button from "../../components/Button";
+import styles from "./styles";
 
 export default function Login() {
   async function handleSignIn() {
@@ -9,20 +10,22 @@ export default function Login() {
   }
 
   return (
-    <View>
+    <View style={styles.container}>
       <KeyboardAvoidingView>
-        <Text>Login</Text>
-        <View>
-          <MaterialIcons name="email" size={24} color="black" />
+        <Text style={styles.title}>Login</Text>
+        <View style={styles.formRow}>
+          <MaterialIcons name="email" style={styles.icon} />
           <TextInput
+            style={styles.input}
             placeholder="E-mail"
             keyboardType="email-address"
             autoCapitalize="none"
           />
         </View>
-        <View>
-          <Entypo name="key" size={24} color="black" />
+        <View style={styles.formRow}>
+          <Entypo name="key" style={styles.icon} />
           <TextInput
+            style={styles.input}
             placeholder="Senha"
             secureTextEntry={true}
             autoCapitalize="none"
