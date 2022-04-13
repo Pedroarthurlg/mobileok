21a553782e2bfd09ab4011353b51fb1a018e665c
lazarodu <lazarodu@gmail.com>
2022-04-13 10:18:26 -0300

    Aula do dia 13/04/2022

--- src/navigations/login.navigation.tsx
+++ src/navigations/login.navigation.tsx
@@ -1,2 +1,15 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
+import { LoginStackParamList } from "../types/Screen.types.ts";
+import { LoginScreen, CadastrarScreen } from "../screens";
+
+const Stack = createStackNavigator<LoginStackParamList>();
+
+export default function LoginNavigation() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Cadastrar" component={CadastrarScreen} />
+    </Stack.Navigator>
+  );
+}
