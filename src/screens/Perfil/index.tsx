8f73b09e5fda7a8015a39da4ae924d75e970c460
lazarodu <lazarodu@gmail.com>
2022-04-20 08:08:55 -0300

    Aula do dia 20/04/2022

--- src/screens/Perfil/index.tsx
+++ src/screens/Perfil/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, ImageBackground } from "react-native";
+import { View, Text, ImageBackground, Image } from "react-native";
 import styles from "./style";
 
 export default function Perfil() {
