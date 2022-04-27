3cdde3f79088e1122d7c67612f72f833e2f62f0a
lazarodu <lazarodu@gmail.com>
2022-04-27 07:49:47 -0300

    Aula do dia 27/04/2022

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -15,7 +15,8 @@ const styles = StyleSheet.create({
   buttonThirty: {
     backgroundColor: colors.thirty,
     borderRadius: 5,
-    margin: 10
+    margin: 10,
+    width: '90%'
   },
   text: {
     color: colors.white,
