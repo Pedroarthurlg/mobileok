2c38766387a307681e8265b1066dd2095dcd6051
lazarodu <lazarodu@gmail.com>
2022-05-04 07:23:16 -0300

    Aula do dia 04/05/2022

--- src/services/data.ts
+++ src/services/data.ts
@@ -30,4 +30,89 @@ export default [
       item: "Reusar"
     }]
   },
+  {
+    id: 3,
+    nome: "Gabriel3",
+    data: "27/04/2022 08:25:00",
+    titulo: "Calça",
+    mensagem: "Tenho uma calça para doar, alguém interessa?",
+    topico: [{
+      id: 1,
+      item: "Reutilizar"
+    }, {
+      id: 2,
+      item: "Doar"
+    }, {
+      id: 3,
+      item: "Reusar"
+    }]
+  },
+  {
+    id: 4,
+    nome: "Gabriel4",
+    data: "27/04/2022 08:25:00",
+    titulo: "Calça",
+    mensagem: "Tenho uma calça para doar, alguém interessa?",
+    topico: [{
+      id: 1,
+      item: "Reutilizar"
+    }, {
+      id: 2,
+      item: "Doar"
+    }, {
+      id: 3,
+      item: "Reusar"
+    }]
+  },
+  {
+    id: 5,
+    nome: "Gabriel5",
+    data: "27/04/2022 08:25:00",
+    titulo: "Calça",
+    mensagem: "Tenho uma calça para doar, alguém interessa?",
+    topico: [{
+      id: 1,
+      item: "Reutilizar"
+    }, {
+      id: 2,
+      item: "Doar"
+    }, {
+      id: 3,
+      item: "Reusar"
+    }]
+  },
+  {
+    id: 6,
+    nome: "Gabriel6",
+    data: "27/04/2022 08:25:00",
+    titulo: "Calça",
+    mensagem: "Tenho uma calça para doar, alguém interessa?",
+    topico: [{
+      id: 1,
+      item: "Reutilizar"
+    }, {
+      id: 2,
+      item: "Doar"
+    }, {
+      id: 3,
+      item: "Reusar"
+    }]
+  },
+  {
+    id: 7,
+    nome: "Gabriel7",
+    data: "27/04/2022 08:25:00",
+    titulo: "Calça",
+    mensagem: "Tenho uma calça para doar, alguém interessa?",
+    topico: [{
+      id: 1,
+      item: "Reutilizar"
+    }, {
+      id: 2,
+      item: "Doar"
+    }, {
+      id: 3,
+      item: "Reusar"
+    }]
+  },
 ]
